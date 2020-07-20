2fb25156b9ee37abe78397d60695e0cc7a444587
jennethydyrova <jennethydyrovaa@gmail.com>
2020-07-20 19:45:54 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+    const userInput = document.getElementById('new-task-description').value;
+    let li = document.createElement('li');
+    li.innerHTML = userInput;
+    taskList.appendChild(li);
+  })
+})
+
+
