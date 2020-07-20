e6721f8d4d9cba87e2a950e790e5fe0561bd1386
betul <bet.er@hotmail.com>
2020-07-20 18:33:37 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.getElementsByTagName("input")[1].addEventListener("click", addNewTodo);
 });
+
+function addNewTodo(event) {
+    event.preventDefault();
+    var ul = document.getElementById("tasks");
+    let li = document.createElement("li");
+    let newTodo = document.getElementById("new-task-description").value;
+    let todoNode = document.createTextNode(newTodo);
+    console.log(newTodo);
+    console.log(todoNode);
+
+    li.appendChild(todoNode);
+    ul.appendChild(li);
+}
