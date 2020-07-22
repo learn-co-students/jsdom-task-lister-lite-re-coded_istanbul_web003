707f77902bed6debc22b84fe43e6cb6f7644af39
fadime94 <fadime.ozdemir11@gmail.com>
2020-07-22 12:55:07 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.getElementsByTagName("input")[1].addEventListener("click", addNewTodo);
+
 });
+
+function addNewTodo(event) {
+  event.preventDefault();
+  var ul = document.getElementById("tasks");
+  let li = document.createElement("li");
+  let newTodo = document.getElementById("new-task-description").value;
+  let todoContent = document.createTextNode(newTodo);
+ 
+
+  li.appendChild(todoContent);
+  ul.appendChild(li);
+}
