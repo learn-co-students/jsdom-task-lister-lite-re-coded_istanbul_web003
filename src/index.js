1375f32e538affbfc2c519dde4942d4f777a57c2
ibrahim aljarad <ibrahimealkalil@gmail.com>
2020-07-23 00:52:23 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  var myTextbox = document.getElementById('new-task-description');
+myTextbox.addEventListener('keypress', checkName, false);
+function checkName(evt) {
+  var charCode = evt.charCode;
+  if (charCode != 0) {
+    if (charCode < 97 || charCode > 122) {
+      evt.preventDefault();
+     
+    }
+  }
+}
+
+
+
+function logSubmit(event) {
+  log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
+  event.preventDefault();
+}
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', logSubmit);
+document.getElementById("create-task-form").addEventListener("submit", (e) => {
+  el.preventDefault();
 });
