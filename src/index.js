259b4c795fabd2b5c4defb272040c47205653855
bertac_severcan <bertac.severcan@icloud.com>
2020-07-24 16:37:39 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const submit = document.querySelector("#submit").addEventListener("click", function(e){
+    e.preventDefault();
+    const input = document.querySelector("#new-task-description");
+    const tasks = document.querySelector("#tasks");
+    tasks.innerHTML += `<li>${input.value}</li> `;
+     
+  });
+  
+
+
 });
