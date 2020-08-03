fc6d46ddda62d109bbdc06c4d2fe60f10f63d471
Hiba <hiba.mashfij@gmail.com>
2020-08-03 22:36:49 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', () => {});
+
+const form = document.getElementById('create-task-form');
+const tasks = document.getElementById('tasks');
+
+
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault()
+
+  const newTask = document.getElementById('new-task-description').value;
+
+
+  tasks.innerHTML += `<li>${newTask}
+  <button class='delete-item'>X</button>
+  </li>`
+
+  form.reset()
+})
+
+
+tasks.addEventListener('click' , function(e) {
+
+ const liDelete = document.querySelector('.delete-item')
+liDelete.onclick = e.target.parentElement.remove()
+
+
+
+})
