e3ca369191e98681dab3ba327e645006231b46fc
mohammad-jundi <mohammad.jundi1990@gmail.com>
2020-10-09 13:53:09 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const input = document.getElementById('create-task-form')
+  const output = document.getElementById('tasks');
+
+   input.addEventListener('submit', function(e) {
+    e.preventDefault()
+    const list = document.getElementById('new-task-description').value;
+
+    output.innerHTML += `<li>${list}</li>`
+     input.reset()
+  })
 });
